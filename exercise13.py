4e5a283dddd86b33adf8e6f6f9ee9e9710143d19
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-23 20:23:33 +0800

    Finished Code!

--- exercise13.py
+++ exercise13.py
@@ -1,6 +1,14 @@
 # FOR i IN range(1, 11):
-#     FOR j IN range(1, 11):
-#         PRINT i * j, followed by a space
-#     PRINT a new line
+for i in range(1, 11):
+    # FOR j IN range(1, 11):
+    for j in range(1, 11):
+            # PRINT i * j, followed by a space
+        print(i * j, end=" ")
+        # PRINT a tab
+    print("\t")
+
+
+
+
 
         
